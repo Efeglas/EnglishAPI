6405a0ddf1fea9bba3a5ab97b16139b19f6677c2
Efeglas <gabor.atty@gmail.com>
2023-07-21 13:25:00 +0200

    Implemented IRequestModel interface

--- EnglishAPI/RequestModels/LoginRequestModel.cs
+++ EnglishAPI/RequestModels/LoginRequestModel.cs
@@ -1,8 +1,35 @@
 ﻿namespace EnglishAPI.RequestModels
 {
-    public class LoginRequestModel
+    public class LoginRequestModel: IRequestModel
     {
         public string Username { get; set; }
         public string Password { get; set; }
+
+        public List<string> Valid()
+        {
+            List<string> errorStrings = new List<string>();
+
+            if (!RequestValidator.notEmpty(Username))
+            {
+                errorStrings.Add("Username must not be empty");
+            }
+
+            if (!RequestValidator.between(Password, 7, 30))
+            {
+                errorStrings.Add("Password must be longer than 7 characters and less than 30");
+            }
+
+            if (!RequestValidator.containsNumber(Password))
+            {
+                errorStrings.Add("Password must contain a number");
+            }
+
+            if (!RequestValidator.containsUppercase(Password))
+            {
+                errorStrings.Add("Password must contain an uppercase letter");
+            }         
+
+            return errorStrings;
+        }
     }
 }
