49bdab1074e7428a9d413fddfb6b5ac4e280142f
Efeglas <gabor.atty@gmail.com>
2023-07-25 19:43:35 +0200

    Added new controller for dictionary

---
+++ EnglishAPI/Controllers/DictionaryController.cs
@@ -0,0 +1,19 @@
+﻿using EnglishAPI.Model;
+using EnglishAPI.RequestModels;
+using EnglishAPI.Response;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EnglishAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class DictionaryController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
