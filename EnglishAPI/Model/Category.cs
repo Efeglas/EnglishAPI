94ffea64164555be73295e0d9414d581ed34981d
Efeglas <gabor.atty@gmail.com>
2023-07-25 19:44:12 +0200

    Added new models

---
+++ EnglishAPI/Model/Category.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EnglishAPI.Model
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        [Column(TypeName = "varchar(50)")]
+        public string Name { get; set; }
+        public ICollection<Word> Words { get; } = new List<Word>();
+
+        [DefaultValue(true)]
+        public bool Visible { get; set; } = true;
+    }
+}
---
+++ EnglishAPI/Model/Word.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EnglishAPI.Model
+{
+    public class Word
+    {
+        public int Id { get; set; }
+        public int CategoryId { get; set; }
+        public int UserId { get; set; }
+        public Category Category { get; set; } = null!;
+        public User User { get; set; } = null!;
+        
+        [Column(TypeName = "varchar(50)")]
+        public string En { get; set; }
+        [Column(TypeName = "varchar(50)")]
+        
+        public string Hu { get; set; }
+
+        [DefaultValue(true)]
+        public bool Visible { get; set; } = true;
+    }
+}
